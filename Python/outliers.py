885283be0d7a75a95babf0b10381aee4acbea462
jason-heller-data <jasonheller84@gmail.com>
2025-02-18 19:41:30 -0500

    Clustering

--- Python/outliers.py
+++ Python/outliers.py
@@ -2,17 +2,42 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.stats as stats
+from sklearn.cluster import DBSCAN
 
 dfMain = pd.read_csv('../aggregate.csv')
 dfMain['Location'] = dfMain['Location'].str.strip()	
-dfMainNoInvoiceLoc = dfMain[dfMain['Location'] != 'AWIN']
-dfMainNoInvoiceLoc['Ring Size'] = dfMainNoInvoiceLoc['Ring Size'].replace('No Size', 0.0).astype(float)
 
-dfDaysPivot = pd.pivot_table(dfMainNoInvoiceLoc, values='Days', index='Ring Size', aggfunc='mean')
-#dfDaysPivot = dfDaysPivot.sort_values(by='Days', ascending=False)
+dfMainNoInvoiceLoc = dfMain[dfMain['Date Received'] != '/  /    ']
 
-print(dfDaysPivot)
-print(dfDaysPivot['Ring Size'].dtypes)
+assert(len(dfMainNoInvoiceLoc) < len(dfMain))
 
-plt.scatter(x=dfDaysPivot['Days'], y=dfDaysPivot['Ring Size'], s = 100)
+#dfMainNoInvoiceLoc['Ring Size'] = dfMainNoInvoiceLoc['Ring Size'].replace('No Size', 0.0).astype(float)
+
+dfDaysPivot = pd.pivot_table(dfMainNoInvoiceLoc, values=['Days', 'Location'], index='Bag #', aggfunc={'Days': np.sum, 'Location': np.count_nonzero})
+dfDaysPivot.reset_index(inplace=True)
+
+dfDaysPivot.describe()
+
+dbscan = DBSCAN(eps=11.0, min_samples=10) # Adjust eps and min_samples as needed
+dfDaysPivot['cluster'] = dbscan.fit_predict(dfDaysPivot[['Days', 'Location']])
+
+uniqueClusters = dfDaysPivot['cluster'].unique()
+colors = plt.cm.viridis(np.linspace(0, 1, len(uniqueClusters))) # Generate a colormap
+
+plt.figure(figsize=(8, 6))
+
+for i, clustedId in enumerate(uniqueClusters):
+
+    cluster_data = dfDaysPivot[dfDaysPivot['cluster'] == clustedId]
+
+    if clustedId == -1:
+        plt.scatter(cluster_data['Days'], cluster_data['Location'], color='gray', label='Outliers')
+    else:
+        plt.scatter(cluster_data['Days'], cluster_data['Location'], color=colors[i], label=f'Cluster {clustedId}')
+
+
+#plt.scatter(x=dfDaysPivot['Days'], y=dfDaysPivot['Location'], s = 10)
+plt.xlabel("Days In Production")
+plt.ylabel("# Locations Visited")
+plt.legend()
 plt.show()
